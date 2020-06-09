7c4264b03ecf693cdd432bd8a28f2ef1081b0e06
mykelalvis <mykel.alvis@gmail.com>
2020-06-09 16:44:07 -0500

    Changes to make api management services less-terrible

--- libraries/azurerm_api_management_service.rb
+++ libraries/azurerm_api_management_service.rb
@@ -4,10 +4,11 @@ require 'azurerm_resource'
 
 class AzurermApiManagementService < AzurermSingularResource
   name 'azurerm_api_management_service'
-  desc 'Verifies settings for an Azure Network DNS Zone'
+  desc 'Verifies settings for an Azure API Management Service'
   example <<-EXAMPLE
     describe azurerm_api_management_service(resource_group: 'rg-1',  name: 'myservicename' ) do
       it { should exist }
+      its('publisherEmail') { should cmp 'jeff@jeff.com' }
     end
   EXAMPLE
 
@@ -16,27 +17,58 @@ class AzurermApiManagementService < AzurermSingularResource
     name
     location
     type
-    publicIpAddresses
-    privateIpAddresses
-    gatewayUrl
-    gatewayRegionalUrl
+    properties
     tags
     etag
   ).freeze
 
   attr_reader(*ATTRS)
 
-  def initialize(resource_group: nil, name:)
-    @name = name
+  def initialize(resource_group: nil, service_name: nil )
+    @name = service_name
     @resource_group = resource_group
-    apims = management.api_management_service(resource_group: resource_group, service_name: name)
+    apims = management.api_management_service( resource_group, service_name)
     return if has_error?(apims)
     assign_fields(ATTRS, apims)
     @exists = true
   end
 
+  def publisherName
+    properties['publisherName'] || nil
+  end
+
+  def publisherEmail
+    properties['publisherEmail'] || nil
+  end
+
+  def publicIpAddresses
+    properties['publicIpAddresses'] || nil
+  end
+
+  def privateIpAddresses
+    properties['privateIpAddresses'] || nil
+  end
+
+  def gatewayUrl
+    properties['gatewayUrl'] || nil
+  end
+
+  def gatewayRegionalUrl
+    properties['gatewayRegionalUrl'] || nil
+  end
+
+  def notificationSenderEmail
+    properties['notificationSenderEmail'] || nil
+  end
+
+  def scmUrl
+    properties['scmUrl'] || nil
+end
+
+
   def to_s
     "Azure API Management Service : #{name}"
   end
+
 
 end
